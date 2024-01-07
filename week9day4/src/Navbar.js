10fe7e3c45fb6aece1f7071b4b11478b6c845fcc
sweetdough <khhrogers@gmail.com>
2024-01-07 04:24:12 -0500

    new update

---
+++ week9day4/src/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return <nav className="nav">
+    
+    <a href="/" className="site-title"> iStocks </a>
+    <ul>
+    <li>
+    <a href="/stocks"> Stocks </a>
+    </li>
+    <li>
+    <a href="/about"> About  </a>
+    </li>
+    
+    </ul>
+    </nav>
+}
