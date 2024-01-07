e59f240b760c96c8cc8390d7308e9516e70e803f
sweetdough <khhrogers@gmail.com>
2024-01-07 05:10:23 -0500

    send

--- week9day4/src/App.js
+++ week9day4/src/App.js
@@ -1,10 +1,25 @@
-import Navbar from "./Navbar";
-
+import Navbar from "./Navbar"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 function App() {
+  let component 
+switch (window.location.pathname) {
+  case "/":
+    component = <Home />
+    break
+    case "/about":
+      component = <About />
+      break
+}
+   return (
+    <> 
+   <Navbar/>
 
-   return <Navbar/>
-  
+   {component}
+   
+   </>
+   )
 }
 
 export default App;
