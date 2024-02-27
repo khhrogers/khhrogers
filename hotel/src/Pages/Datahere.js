42d434d1ce85d998033acc02cd4b235259d0b0c4
sweetdough <khhrogers@gmail.com>
2024-02-27 09:52:58 -0500

    creating api
    this api shows travel advisories versus the hotel api that did not work

---
+++ hotel/src/Pages/Datahere.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+
+const APIComponent = () => {
+    const [advisoryInfo, setAdvisoryInfo] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
+
