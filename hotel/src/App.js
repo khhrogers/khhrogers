8fb6c00503bc20dcc55f51603fe21ae0451af1a8
sweetdough <khhrogers@gmail.com>
2024-02-27 09:55:08 -0500

    show api on page

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Datahere from './Pages/Datatime'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     // api will display here 
+      <Datahere />
+       
     </div>
   );
 }
