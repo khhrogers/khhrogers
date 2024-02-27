3d50d82c0450f28d12edd25384505da7b7ece1e1
sweetdough <khhrogers@gmail.com>
2024-02-27 10:09:57 -0500

    building fetch
    building fetch

--- hotel/src/Pages/Datahere.js
+++ hotel/src/Pages/Datahere.js
@@ -6,3 +6,18 @@ const APIComponent = () => {
     const [error, setError] = useState(null);
 }
 
+useEffect(() => {
+const fertData = async () => [
+try {
+    const response = await fetch('https://www.travel-advisory.info/api?countrycode=US')
+    
+}
+
+
+]
+
+
+
+
+}
+   
