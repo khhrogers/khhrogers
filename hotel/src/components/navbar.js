c9aa1c2711ef198aa0a1826a14bdf4b7c3a1f731
sweetdough <khhrogers@gmail.com>
2024-02-28 21:06:58 -0500

    log out function
    users can now log out

--- hotel/src/components/navbar.js
+++ hotel/src/components/navbar.js
@@ -1,12 +1,25 @@
 import {Link} from "react-router-dom";
+import React from 'react';
+import * as userService from '../components/utilities/user-service'
 
-export default function Navbar () {
-    return (
+export default function Navbar ({user, setUser}) {
+   
+    function handleLogOut() {
+        userService.logOut()
+        // Update App.js state to null
+        setUser(null)
+      }
+    
+   
+   
+   return (
 <nav>
 
 <Link to="/travel">Search Hotel</Link>
       &nbsp; | &nbsp;
       <Link to="/travel/alert">Alerts</Link>
+      &nbsp;&nbsp;<span>Welcome, {user.name}</span>
+      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
   </nav>
     );
 }
